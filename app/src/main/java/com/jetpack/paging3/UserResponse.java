c88ce532acf89e56e83e77c1fc1b6a0eff2fa267
fly <lanseyoukong638@163.com>
2022-03-11 10:23:30 +0800

    逻辑完善和bug修复

--- app/src/main/java/com/jetpack/paging3/UserResponse.java
+++ app/src/main/java/com/jetpack/paging3/UserResponse.java
@@ -18,17 +18,22 @@ public class UserResponse {
     }
 
     private static User getUser(int i) {
-        return new User(i,"name" + i,i);
+        User user = new User(i,"name" + i,i);
+        if(i >= Integer.MAX_VALUE){
+            i = 0;
+        }
+        return user;
     }
 
 
+    static int index = 0;
     public static List<User> getUsersForRoom(int page,int pageSize){
         try {Thread.sleep(900);} catch (InterruptedException e) {e.printStackTrace();}
         Log.d("debug_log","UserResponse --- getUsers page = " + page + " pageSize = " + pageSize +" --- " + Thread.currentThread().getName());
         int size = page + pageSize;
         List<User> items = new ArrayList<>();
         for (;page<size;page++){
-            items.add(getUser(page));
+            items.add(getUser(index++));
         }
         return items;
     }
